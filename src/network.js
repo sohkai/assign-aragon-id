d945e97a916b0dc15d8c6e70c25d5c8ba87df562
Brett Sun <qisheng.brett.sun@gmail.com>
2020-02-19 16:34:34 +0100

    feat: add ropsten environment

--- src/network.js
+++ src/network.js
@@ -1,4 +1,3 @@
-const acceptedNetworks = ['mainnet', 'rinkeby']
 const networkConfigMap = {
   mainnet: {
     ensRegistry: '0x314159265dd8dbb310642f98f50c066173c1259b',
@@ -10,7 +9,13 @@ const networkConfigMap = {
     aragonIdRegistry: '0x3665e7bfd4d3254ae7796779800f5b603c43c60d',
     node: 'wss://rinkeby.eth.aragon.network/ws',
   },
+  ropsten: {
+    ensRegistry: '0x6afe2cacee211ea9179992f89dc61ff25c61e923',
+    aragonIdRegistry: '0x3c7f5e7946c09c38f53d608adfd2ed5d6b137ade',
+    node: 'wss://ropsten.eth.aragon.network/ws',
+  }
 }
+const acceptedNetworks = Object.keys(networkConfigMap)
 
 module.exports = {
   acceptedNetworks,
